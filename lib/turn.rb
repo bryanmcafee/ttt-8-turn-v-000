35d8defb3464d2897d821b354f78866e1c0fb895
Bryan McAfee <brymcafee@gmail.com>
2017-11-01 14:09:17 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,4 +6,14 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-  
+def valid_move?(board, position)
+move=position.to_i
+test = move - 1
+test.between?(0,8) && !(position_taken?(board, test))
+end
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, position)
+return false if [" ", ""].include?(board[position])
+return true if ["X", "O"].include?(board[position])
+end
+
