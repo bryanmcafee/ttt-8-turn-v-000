7c06f527d408d472f71a13a8edf7369afd3a0194
Bryan McAfee <brymcafee@gmail.com>
2017-11-01 14:13:32 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,3 +16,14 @@ def position_taken?(board, position)
 return false if [" ", ""].include?(board[position])
 return true if ["X", "O"].include?(board[position])
 end
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(board, index, token ="X")
+  board[index] = token
+  board
+end
+
+
